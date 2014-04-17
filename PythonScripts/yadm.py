224dae473a08467572a3376d7c5d25bbe5e587be
Adam Zabell <adam.zabell@gmail.com>
2014-04-16 23:23:17 -0400

    add sys.argv

--- PythonScripts/yadm.py
+++ PythonScripts/yadm.py
@@ -1,42 +1,40 @@
 #apz#!/usr/bin/python
-from core_variables import *
+import sys
 import room
 
-#ARGV.length < 1 ? fail("\n You need to specify the dungeon level") : actual_level = ARGV[0].to_i
-#ARGV.length > 2 ? fail("\n Only two input variables, please") : room_size = ARGV[1].to_i
-dungeon_level = 3
-room_size = 1
-wandering_monster = True
+whats_here = room.Description.parse_input(sys.argv)
+if "ERROR" in whats_here:
+    sys.exit(whats_here)
+room_size = whats_here["room"] if "room" in whats_here else False
+dungeon_level = whats_here["level"] if "level" in whats_here else False
+wandering_monster = whats_here["wander"] if "wander" in whats_here else False
 
 ### Doors
-if room_size > 0:
+if room_size:
     number_of_doors = room.Door.door_count()
-    print("\n~~~ In this %i sq ft room there are %i other exits:" % (room_size, number_of_doors))
+    print("~~~ In this %i sq ft room there are %i other exits:" % (room_size, number_of_doors))
     for n in list(range(0, number_of_doors)):
         this_door = room.Door
         egress = this_door.opening()
         egress_text = " ".join([this_door.latched(egress),this_door.constructed_with(egress),egress]).lstrip(" ")
         print(" (%i) A %s on the %s wall." % (n+1, egress_text, this_door.location()) )
         if egress_text != "portal": print(this_door.game_details(egress_text))
-    print("\n")
 
 ### Contents
-this_room = room.Contents.master_list()
-if wandering_monster: this_room = ("monster")
-if "special" in this_room:
-    print("~~~ Special: reroll, or staircase, or Shop\n\n")
-if "nothing" in this_room:
-    print("~~~ Empty room. Or is it...\n\n")
-if "feature" in this_room:
-    print(room.Feature.information())
-if "monster" in this_room:
-    print(room.Monster.information(dungeon_level))
-if "hidden_treasure" in this_room:
-    print(room.Treasure.information(dungeon_level))
-if "trap" in this_room:
-    print(room.Trap.information(dungeon_level))
-
-### Wandering Monsters
-if wandering_monster:
-    print(room.Monster.wandering_monster_check(dungeon_level))
-
+this_room = room.Description.of_what_is_here()
+if dungeon_level > 0:
+    if wandering_monster:
+        print(room.Monster.wandering_monster_check(dungeon_level))
+        sys.exit()
+    if "special" in this_room:
+        print("~~~ Special: reroll, or staircase, or Shop")
+    if "nothing" in this_room:
+        print("~~~ Empty room. Or is it...")
+    if "feature" in this_room:
+        print(room.Feature.information())
+    if "monster" in this_room:
+        print(room.Monster.information(dungeon_level))
+    if "hidden_treasure" in this_room:
+        print(room.Treasure.information(dungeon_level))
+    if "trap" in this_room:
+        print(room.Trap.information(dungeon_level))
